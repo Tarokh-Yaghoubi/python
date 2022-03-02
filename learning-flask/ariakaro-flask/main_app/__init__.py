dc8beb3f77e0651e99df998a66e83e3b6eaaaa01
tarokh <tarokhyaghoubi@yahoo.com>
2022-03-02 23:24:25 +0330

    Modified

--- learning-flask/ariakaro-flask/main_app/__init__.py
+++ learning-flask/ariakaro-flask/main_app/__init__.py
@@ -1,26 +1,26 @@
-import sqlalchemy
+import mysql.connector
 from flask import Flask
 from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
-from sqlalchemy import create_engine
-from sqlalchemy_utils import create_database, database_exists 
+from mysql.connector import Connect, Error
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/management"
-#app.config['SECRET_KEY'] = "SPM5axwkh|nl@L2PcQm$uUgwBtu#JKIfH1f"
-db = SQLAlchemy(app)
-engine = sqlalchemy.create_engine("mysql+pymysql://root:@localhost/management", convert_unicode=True)
-conn = engine.connect()
-res = db.select()
 
-Base = declarative_base()
-db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-session = db_session()
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="management"
+)
 
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM users")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    ...
 
 import main_app.views
 import main_app.models
